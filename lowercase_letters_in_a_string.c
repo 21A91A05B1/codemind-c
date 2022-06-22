a423f77d8bd270a81606bb0f9fa6e3b169613e2b
21A91A05B1 <97426126+21A91A05B1@users.noreply.github.com>
2022-06-22 22:54:51 +0530

    #include <stdio.h> int main() {     char str[100];     int i,c=0;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]>='a' && str[i]<='z')         {             c++;         }     }     printf("%d",c); }

---
+++ lowercase_letters_in_a_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int i,c=0;
+    scanf("%[^
+]s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]>='a' && str[i]<='z')
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
