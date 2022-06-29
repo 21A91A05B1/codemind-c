09d78851863c45ba8651b3516b63df96f72cb5a7
21A91A05B1 <97426126+21A91A05B1@users.noreply.github.com>
2022-06-29 15:07:55 +0530

    #include<stdio.h> int main() {     int i,n,t,arr[100],c=0,min;     scanf("%d",&t);     while(t--)     {         c=0;         scanf("%d",&n);         for(i=0;i<n;i++)         {             scanf("%d",&arr[i]);         }         min=arr[0];         for(i=0;i<n;i++)         {             if(arr[i]<=min)             {                 c++;                 min=arr[i];             }         }         printf("%d ",c);     } }

---
+++ Speed.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,t,arr[100],c=0,min;
+    scanf("%d",&t);
+    while(t--)
+    {
+        c=0;
+        scanf("%d",&n);
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&arr[i]);
+        }
+        min=arr[0];
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]<=min)
+            {
+                c++;
+                min=arr[i];
+            }
+        }
+        printf("%d
+",c);
+    }
+}
