4450e09f522b5d07bce6a2869806812811fa6689
21A91A05B1 <97426126+21A91A05B1@users.noreply.github.com>
2022-06-29 19:52:58 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,flag=0,t;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&t);     for(i=0;i<n;i++)     {         if(arr[i]==t)         {             flag=1;             printf("%d ",i);             break;         }     }     if(flag==0)     {         printf("-1");     } }

---
+++ Search_in_Rotated_Sorted_Array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,flag=0,t;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&t);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==t)
+        {
+            flag=1;
+            printf("%d ",i);
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        printf("-1");
+    }
+}
