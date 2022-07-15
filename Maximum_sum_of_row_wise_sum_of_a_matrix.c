11e84f18027fd9eac9199c3534f226377722ac23
21A91A05B1 <97426126+21A91A05B1@users.noreply.github.com>
2022-07-15 22:57:16 +0530

    #include<stdio.h> int main() {     int i,j,m,n,a[100][100],s=0,max=0;     scanf("%d %d",&m,&n);     for(i=0;i<m;i++)     {         for(j=0;j<n;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<m;i++)     {         s=0;         for(j=0;j<n;j++)         {             s=s+a[i][j];         }         if(s>max)         {             max=s;         }     }     printf("%d",max); }

---
+++ Maximum_sum_of_row_wise_sum_of_a_matrix.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,m,n,a[100][100],s=0,max=0;
+    scanf("%d %d",&m,&n);
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<m;i++)
+    {
+        s=0;
+        for(j=0;j<n;j++)
+        {
+            s=s+a[i][j];
+        }
+        if(s>max)
+        {
+            max=s;
+        }
+    }
+    printf("%d",max);
+}
